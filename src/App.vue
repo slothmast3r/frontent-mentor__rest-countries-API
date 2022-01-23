a93213877c2bab4750a6976ce5d6ab4adc9736e6
slothmast3r <oskar.straszynski@gmail.com>
2022-01-23 01:52:12 +0100

    where in the world??

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="content-wrapper">
+    <div class="header">
+      <div class="header-title">
+        Where in the world?
+      </div>
+    </div>
+    <div class="body">
+
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
 
 @Options({
   components: {
-    HelloWorld,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,600;0,800;1,300&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Nunito Sans', sans-serif;
+}
+.header-title{
+  padding: 1em 3em;
+  font-weight: 800;
+  font-size: 1.5rem;
 }
 </style>
